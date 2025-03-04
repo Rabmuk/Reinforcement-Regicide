e8a7b6628722316c7df62d737286ac5f03630fb0
Alex K <Rabmuk@users.noreply.github.com>
2025-03-04 14:43:16 -0500

    Attack enemy and partial suit effect implementation. has not been tested

--- regicide.py
+++ regicide.py
@@ -47,6 +47,7 @@ class Card:
             return int(self.value)
         
     def check_card_command(self, command:str)->bool:
+        command = command.upper()
         if not command.startswith(self.value):
             return False
         command = command.strip(self.value)
@@ -97,6 +98,9 @@ class Deck:
     def add_card(self, card):
         self.cards.append(card)
 
+    def add_card_on_top(self, card):
+        self.cards.insert(0, card)
+
     def shuffle(self):
         random.shuffle(self.cards)
 
@@ -113,17 +117,22 @@ class Player:
     def __str__(self):
         return f'{self.name}\n{self.show_hand()}'
 
-    def draw_from_deck(self, deck, number=1):
+    def draw_from_deck(self, deck, number=1)->bool:
+        """Draws a specified number of cards from the deck and adds them to the player's hand.
+        If the player doesn't draw number cards, due to hand limit or empty deck, returns False."""
         for _ in range(number):
             if len(self.hand) >= self.hand_limit:
                 print(f'{self.name} is at hand limit of {self.hand_limit}')
-                break
+                return False
             card = deck.draw_card()
             if card:
                 self.hand.append(card)
                 print(f"{self.name} has drawn: {card}")
             else:
                 print("No more cards in the deck.")
+                return False
+            
+        return True
 
     def show_hand(self, sorting=None)->str:
         self.hand = sorted(self.hand)
@@ -139,6 +148,7 @@ class Player:
 
     def parse_and_check_command(self, command):
         """Takes a str and verifies if it is formatted correctly and that this player has the required cards."""
+        command = command.replace(' ', '').replace(',', '').replace('.', '')
         commands = []
         while len(command) > 0:
             if command.startswith('10'):
@@ -147,20 +157,33 @@ class Player:
             else:
                 commands.append(command[:2])
                 command = command[2:]
-        if all(self.has_card(cmd) for cmd in commands):
+
+        missing_cmd = None
+        if all(self.has_card(missing_cmd:=cmd) for cmd in commands):
             return commands
         else:
-            return False
+            raise ValueError(f"Invalid command: {missing_cmd}. Incorrect format or card is unavailable.")
 
-    def play_cards(self, command):
-        pass
+    def play_cards(self, commands)->list[Card]:
+        """Returns list of cards specified in the commands.
+        Removes the cards from the player's hand."""
+        cards = []
+        for command in commands:
+            for card in self.hand:
+                if card.check_card_command(command):
+                    cards.append(card)
+                    self.hand.remove(card)
+                    break
+
+        return cards
 
 class RegicideGame:
     def __init__(self, player_names=['a','b']):
-        self.turn = 0
+        self.turn = 1
         self.deck = Deck(deck_type='Tavern', shuffle=True)
         self.players = [Player(name=n) for n in player_names]
-        self.active_player = self.players[0]
+        self.active_player_index = 0
+        self.active_player = self.players[self.active_player_index]
         self.enemies = Deck(deck_type='Castle')
         self.discard = Deck(deck_type='Empty')
         self.running = True
@@ -197,14 +220,6 @@ class RegicideGame:
             
         return False
 
-    def get_card_value(self, card):
-        if card.value in ['J', 'Q', 'K']:
-            return {'J': 10, 'Q': 15, 'K': 20}[card.value]
-        elif card.value == 'A':
-            return 1
-        else:
-            return int(card.value)
-
     def play_card(self, card_index):
         if not self.current_enemy:
             print("No enemy to attack!")
@@ -224,28 +239,145 @@ class RegicideGame:
 
         self.player.hand.pop(card_index)
 
+    def refill_tavern(self, number):
+        """Shuffle discard then add specified number of cards from the discard to the bottom of the tavern deck."""
+        print(f"\nRefilling the tavern with {number} cards.")
+        
+        self.discard.shuffle()
+        
+        for _ in range(number):
+            if self.discard.cards:
+                card = self.discard.draw_card()
+                self.deck.add_card(card)
+            else:
+                print("No more cards in the discard pile.")
+                break
+
+        print(f"There are {len(self.discard)} cards in the discard pile.")
+        print(f"There are {len(self.deck)} cards in the tavern deck.")
+        
+    def deal_to_players(self, number):
+        """Deals a specified number of cards, cycling through the recieving player."""
+        print(f"\nDealing {number} cards to players.")
+        
+        player_index = self.active_player_index
+        for _ in range(number):
+            self.players[player_index].draw_from_deck(self.deck)
+            player_index = (player_index + 1) % len(self.players)
+
+    def attack_enemy(self, cards:list[Card]):
+        """Takes a list of cards, deals damage to enemy and applies suit effects."""
+        assert self.current_enemy, "No enemy to attack!"
+
+        cards_value = sum(self.get_card_value(card) for card in cards)
+
+        # apply suit effect clubs
+        double_damage = (
+            self.current_enemy.suit != 'Clubs' and
+            any(card.suit == 'Clubs' for card in cards)
+        )
+        # assign damage
+        total_damage = cards_value * (2 if double_damage else 1)
+        print(f"\nYou played {', '.join(str(card) for card in cards)} for {total_damage} damage.")
+        
+        self.current_enemy.health -= total_damage
+
+        # apply suit effect spades
+        reduce_attack = (
+            self.current_enemy.suit != 'Spades' and
+            any(card.suit == 'Spades' for card in cards)
+        )
+        if reduce_attack:
+            self.current_enemy.attack -= cards_value
+            self.current_enemy.attack = 0 if self.current_enemy.attack < 0 else self.current_enemy.attack
+            print(f"Enemy attack has been reduced by {cards_value}." )
+            print(f"Enemy attack: {self.current_enemy.attack}")
+            
+        
+        # apply suit effect hearts
+        # important to apply hearts before diamonds
+        refill_tavern = (
+            self.current_enemy.suit != 'Hearts' and
+            any(card.suit == 'Hearts' for card in cards)
+        )
+        if refill_tavern:
+            self.refill_tavern(cards_value)
+        
+        # apply suit effect diamonds
+        draw_cards = (
+            self.current_enemy.suit != 'Diamonds' and
+            any(card.suit == 'Diamonds' for card in cards)
+        )
+        if draw_cards:
+            self.deal_to_players(cards_value)
+
+        # check if enemy is defeated
+        if self.current_enemy.health <= 0:
+            print(f"You defeated {self.current_enemy}!")
+
+            if self.current_enemy.health == 0:
+                print("Exact damage! Defeated Enemy added to the top of Tavern Deck")
+                self.deck.add_card_on_top(self.current_enemy)
+            else:
+                print("Defeated enemy will go to the discard pile!")
+                self.discard.add_card(self.current_enemy)
+
+            self.next_enemy()
+        else:
+            print(f"{self.current_enemy} has {self.current_enemy.health} health remaining.")
+
     def play_game(self):
         while self.running:
-            while self.is_player_turn:
-                print()
-                print(f'It is {self.active_player.name}\'s turn')
-                print(self.active_player.show_hand())
-                command = input("\ntype card(s) to play them, 'yield' to not attack, or 'quit' to quit: ").lower()
-                print("\n")
+            print(f"\nTurn {self.turn}")
+            self.player_turn()
+            if not self.check_victory():
+                self.enemy_turn()
+                self.turn += 1
+
+    def check_victory(self):
+        # TODO: create victory conditions
+        return False
+
+    def player_turn(self):
+        print()
+        print(f'It is {self.active_player.name}\'s turn')
+        print(self.active_player.show_hand())
+
+        valid_play = False
+        while not valid_play:
+            command = input("\ntype card(s) to play them, 'yield' to not attack, or 'quit' to quit: ").lower()
+            print("\n")
+            
+            if command == 'quit':
+                print("Thanks for playing!")
+                self.running = False
+                return
                 
-                if command == 'quit':
-                    print("Thanks for playing!")
-                    self.running = False
+            elif command == 'yield':
+                print("You chose not to attack this turn.")
+                self.is_player_turn = False
+                valid_play = True
+
+            else:
+                cmd_list = None
+                try:
+                    cmd_list = self.active_player.parse_and_check_command(command)
+                except ValueError as e:
+                    print(e)
+
+                if cmd_list:
+                    played_cards = self.active_player.play_cards(cmd_list)
+                    self.attack_enemy(played_cards)
                     
-                elif command == 'yield':
-                    print("You chose not to attack this turn.")
-                    self.enemy_attack()
-                elif self.active_player.can_play(command):
-                    cards = self.player.get_cards(command)
-                else:
-                    print("Invalid command.")
-            while not self.is_player_turn:
-                pass
+                    self.is_player_turn = False
+                    valid_play = True
+
+        self.next_player()
+
+    def enemy_turn(self):
+        
+        
+        self.is_player_turn = True
 
 if __name__ == "__main__":
     game = RegicideGame(player_names=['Alice','Bob'])
